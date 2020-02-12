364ea7a4cf8f24d11f0e81de82125f22a89b66fe
ZC <zhoucan1999@outlook.com>
2020-02-12 12:27:17 +0000

    GitHub test

--- app/src/androidTest/java/com/tcd3d5b/bookingsystem/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/tcd3d5b/bookingsystem/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.tcd3d5b.bookingsystem;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- app/src/main/java/com/tcd3d5b/bookingsystem/MainActivity.java
+++ app/src/main/java/com/tcd3d5b/bookingsystem/MainActivity.java
@@ -1,9 +1,12 @@
 package com.tcd3d5b.bookingsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    // GitHub test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

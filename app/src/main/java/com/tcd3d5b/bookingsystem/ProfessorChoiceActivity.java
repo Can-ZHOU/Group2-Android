ea1e0c8218d2aa95275206b70e083a96b2ad600e
ZC <zhoucan1999@outlook.com>
2020-02-21 15:11:39 +0000

    Add professor

--- app/src/main/java/com/tcd3d5b/bookingsystem/ProfessorChoiceActivity.java
+++ app/src/main/java/com/tcd3d5b/bookingsystem/ProfessorChoiceActivity.java
@@ -9,45 +9,46 @@ import android.widget.Button;
 
 import com.tcd3d5b.bookingsystem.game2048.Game2048MainActivity;
 
-public class ChoiceActivity extends AppCompatActivity {
+public class ProfessorChoiceActivity extends AppCompatActivity {
     Button userProfile, booking, professorTimetable, game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choice);
+        setContentView(R.layout.activity_professor_choice);
 
-        userProfile = findViewById(R.id.button_profile);
-        booking = findViewById(R.id.button_booking);
-        professorTimetable = findViewById(R.id.button_professor_timetable);
-        game = findViewById(R.id.button_game);
+        userProfile = findViewById(R.id.button_professor_profile);
+        booking = findViewById(R.id.button_professor_glassroom);
+        professorTimetable = findViewById(R.id.button_professor_add_time);
+        game = findViewById(R.id.button_professor_game);
 
         userProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ChoiceActivity.this, UserProfileActivity.class));
+                startActivity(new Intent(ProfessorChoiceActivity.this, UserProfileActivity.class));
             }
         });
 
         booking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ChoiceActivity.this, BookingActivity.class));
+                startActivity(new Intent(ProfessorChoiceActivity.this, BookingActivity.class));
             }
         });
 
         professorTimetable.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ChoiceActivity.this, ProfessorTimetableActivity.class));
+                startActivity(new Intent(ProfessorChoiceActivity.this, ProfessorTimetableActivity.class));
             }
         });
 
         game.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ChoiceActivity.this, Game2048MainActivity.class));
+                startActivity(new Intent(ProfessorChoiceActivity.this, Game2048MainActivity.class));
             }
         });
     }
+
 }

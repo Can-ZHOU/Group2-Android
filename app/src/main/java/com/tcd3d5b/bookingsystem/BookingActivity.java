7d8d20152ae13fcc878834f566fff7587051ff5d
Rohan Taneja <tanejar@tcd.ie>
2020-02-28 16:31:48 +0000

    BookingActivity: merge with CAN's code & add fixes
    Signed-off-by: Rohan Taneja <tanejar@tcd.ie>

--- app/src/main/java/com/tcd3d5b/bookingsystem/BookingActivity.java
+++ app/src/main/java/com/tcd3d5b/bookingsystem/BookingActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,7 +17,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 public class BookingActivity extends AppCompatActivity {
@@ -38,9 +42,15 @@ public class BookingActivity extends AppCompatActivity {
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
-                // check whether input date and time are valid.
-                search(date.getText().toString(), time.getText().toString());
+                // Check Formatting for Date & Time
+                if (checkValid(date.getText().toString())) {
+                    if (time.getText().toString().matches("(1[0-9]|2[0-1]):(00)")) {
+                        search(date.getText().toString(), time.getText().toString());
+                    } else
+                        Toast.makeText(getApplicationContext(), "TimeFormatError: Use {XX:00} Format", Toast.LENGTH_LONG).show();
+                }
+                else
+                    Toast.makeText(getApplicationContext(), "DateFormatError: Use {20YY-MM-DD} Format", Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -58,25 +68,23 @@ public class BookingActivity extends AppCompatActivity {
                 if( !dataSnapshot.exists() || !dataSnapshot.hasChildren()) {
                     roomNumber = 1;
                 } else {
-                    ArrayList<String> roomList = new ArrayList<String>();
+                    ArrayList<String> roomList = new ArrayList<>();
                     for (DataSnapshot room : dataSnapshot.getChildren()) {
                         roomList.add(room.getValue().toString());
                         // for debug
                         Log.i(TAG, "onDataChange: room number : " + room.getValue());
                     }
-
                     roomNumber = roomList.size()+1;
-                    Log.i(TAG, "onDataChange: roomList size: "+roomList.size());
-
-                    // Jump to AvailableGlassroomActivity
-                    startActivity(new Intent(BookingActivity.this, AvailableGlassroomActivity.class));
                 }
-
-                if (flag==false) {
+                if(roomNumber <= 9 && !flag){
                     flag = true;
                     addRooom(date.getText().toString(), time.getText().toString(), Integer.toString(roomNumber));
+                    Toast.makeText(getApplicationContext(), "Confirmed: " + date.getText().toString()  + time.getText().toString() + "Room: " + (char) roomNumber, Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(BookingActivity.this, ConfirmationActivity.class));
                 }
-
+                else{
+                    Toast.makeText(getApplicationContext(), "RoomError: Select another date", Toast.LENGTH_LONG).show();
+                }
             }
 
             @Override
@@ -88,5 +96,32 @@ public class BookingActivity extends AppCompatActivity {
 
     private void addRooom(String date, String time, String roomNumber) {
         DBref.child("glassroom").child("date").child(date).child(time).child(roomNumber).setValue(roomNumber);
+    }
+    public static boolean checkValid(String strDate)
+    {
+        /* Check if date is 'null' */
+        if (strDate.trim().equals(""))
+        {
+            return true;
+        }
+        /* Date is not 'null' */
+        else
+        {
+            SimpleDateFormat strdate = new SimpleDateFormat("yyyy-MM-dd");
+            strdate.setLenient(false);
+            try
+            {
+                Date javaDate = strdate.parse(strDate);
+                System.out.println(strDate+" is valid date format");
+            }
+            /* Date format is invalid */
+            catch (ParseException e)
+            {
+                System.out.println(strDate+" is Invalid Date format");
+                return false;
+            }
+            /* Return true if date format is valid */
+            return true;
+        }
     }
 }

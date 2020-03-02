0811d30e1d6f08ac1b49f60ed6d9731e3167b092
ZC <zhoucan1999@outlook.com>
2020-03-02 21:18:29 +0000

    add checking valid date and time function in booking glassroom which is from Rohan

---
+++ app/src/main/java/com/tcd3d5b/bookingsystem/ConfirmationGlassroomActivity.java
@@ -0,0 +1,15 @@
+package com.tcd3d5b.bookingsystem;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfirmationGlassroomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation_glassroom);
+
+    }
+}

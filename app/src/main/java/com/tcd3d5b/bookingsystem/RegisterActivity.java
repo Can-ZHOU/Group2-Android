a084c66bc5f973ff62d8eb4aa2d238738b0e2de0
ujjawal61 <aggarwau@tcd.ie>
2020-03-03 22:39:40 +0000

    completed register page

--- app/src/main/java/com/tcd3d5b/bookingsystem/RegisterActivity.java
+++ app/src/main/java/com/tcd3d5b/bookingsystem/RegisterActivity.java
@@ -50,7 +50,7 @@ public class RegisterActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                startActivity(new Intent(RegisterActivity.this, ChoiceActivity.class));
+                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
                             }
                         }
                     });
